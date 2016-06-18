a42736a031bd36246acdf1d5cf2035a701630ba1
Harley Eades <heades@gru.edu>
2016-06-18 12:32:23 -0400

    Converted internal state to a queue of pairs instead of a queue of REPLExpr's because the former is safer.

--- impl/Repl.hs
+++ impl/Repl.hs
@@ -11,46 +11,35 @@ import Syntax
 import Parser
 import Pretty
 import TypeChecker
-            
-type REPLStateIO = StateT (Queue REPLExpr) IO
 
+type Qelm = (Vnm, Term)
+type REPLStateIO = StateT (Queue Qelm) IO
 instance MonadException m => MonadException (StateT s m) where
     controlIO f = StateT $ \s -> controlIO $ \(RunIO run) -> let
                     run' = RunIO (fmap (StateT . const) . run . flip runStateT s)
                     in fmap (flip runStateT s) $ f run'
-
--- Error messages:
-inconsistentState = "Error: internal state is not consistent: state should contain only top-level definitions."                       
+                
 io :: IO a -> REPLStateIO a
 io i = liftIO i
     
 pop :: REPLStateIO (Vnm, Term)
-pop = get >>= io.caseLet.(headQ)
- where
-   caseLet :: REPLExpr -> IO (Vnm, Term)
-   caseLet (Let n t) = return $ (n , t)
-   caseLet _ = fail inconsistentState
+pop = get >>= return.headQ
 
-push :: REPLExpr -> REPLStateIO ()
-push (t@(Let _ _)) = get >>= put.(`snoc` t) 
-push _ = io $ fail inconsistentState
+push :: Qelm -> REPLStateIO ()
+push t = get >>= put.(`snoc` t) 
          
-unfoldDefsInTerm :: (Queue REPLExpr) -> Term -> Term
+unfoldDefsInTerm :: (Queue Qelm) -> Term -> Term
 unfoldDefsInTerm q t =
-    let uq = toListQ $ mapQ toPair (unfoldQueue q)
+    let uq = toListQ $ unfoldQueue q
      in substs uq t
- where
-   toPair (Let x t) = (x , t)
-   toPair _ = error inconsistentState
 
-unfoldQueue :: (Queue REPLExpr) -> (Queue REPLExpr)
+unfoldQueue :: (Queue Qelm) -> (Queue Qelm)
 unfoldQueue q = fixQ q emptyQ step
  where
-   step e@(Let x t) _ r = (mapQ (substREPLExpr x t) r) `snoc` e
+   step e@(x,t) _ r = (mapQ (substDef x t) r) `snoc` e
     where
-      substREPLExpr :: Name Term -> Term -> REPLExpr -> REPLExpr
-      substREPLExpr x t (Let y t') = Let y $ subst x t t'
-   step _ _ _ = error inconsistentState
+      substDef :: Name Term -> Term -> Qelm -> Qelm
+      substDef x t (y, t') = (y, subst x t t')
 
 handleCMD :: String -> REPLStateIO ()
 handleCMD "" = return ()
@@ -60,16 +49,13 @@ handleCMD s =
       Right l -> handleLine l
   where
     handleLine :: REPLExpr -> REPLStateIO ()
-    handleLine (t@(Let _ _)) = push t
-    handleLine (TypeCheck t) = get >>=
-                                 (\defs -> let ty = typeCheck.unfoldDefsInTerm defs $ t
-                                           in io $ putStrLn "Type checking: not implemented yet.")
+    handleLine (Let x t) = push (x , t)
+    handleLine (TypeCheck t) = get >>= (\defs -> io.putStrLn.prettyType.typeCheck.unfoldDefsInTerm defs $ t)
     handleLine (ShowAST t) = io.putStrLn.show $ t
     handleLine (Unfold t) = get >>= (\defs -> io.putStrLn.runPrettyTerm $ unfoldDefsInTerm defs t)
-    handleLine DumpState = get >>= io.print.(mapQ prettyREPLExpr)
+    handleLine DumpState = get >>= io.print.(mapQ prettyDef)
      where
-       prettyREPLExpr (Let x t) = "let "++(n2s x)++" = "++(runPrettyTerm t)
-       prettyREPLExpr _ = inconsistentState
+       prettyDef (x, t) = "let "++(n2s x)++" = "++(runPrettyTerm t)
 
 banner :: String
 banner = "Welcome to Grady!\n\nThis is the gradual typing from a categorical perspective repl.\n\n"
--- impl/TypeChecker.hs
+++ impl/TypeChecker.hs
@@ -1,6 +1,11 @@
-module TypeChecker where
+module TypeChecker (typeCheck) where
 
 import Syntax
 
+type TyCtx = [(Vnm, Term)]
+
 typeCheck :: Term -> Type
-typeCheck t = Nat
+typeCheck t = runFreshM $ typeCheck_aux [] t
+
+typeCheck_aux :: Fresh m => TyCtx -> Term -> m Type
+typeCheck_aux ctx t = return Nat
