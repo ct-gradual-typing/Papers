635657deb21751e0849e6d908427fd9d30536434
michael_townsend <michael.h.townsend@gmail.com>
2016-09-12 08:32:52 -0400

    initial changes to syntax

--- impl/Syntax.hs
+++ impl/Syntax.hs
@@ -9,7 +9,7 @@ module Syntax (module Unbound.LocallyNameless,
 import Unbound.LocallyNameless 
 import Unbound.LocallyNameless.Alpha
       
-data Type =                     -- Types:
+data Type =               -- Types:
    Nat                          -- Natural number type
  | Unit                         -- Unit type
  | U                            -- Untyped universe
@@ -22,8 +22,12 @@ type Vnm = Name Term            -- Variable name
 data Term =
    Var Vnm                      -- Free varialbe
  | Triv                         -- Unit's inhabitant
- | Gen Type                     -- Generalize
- | Spec Type                    -- Specialize   
+ | Sqsh							-- Injection of the retract
+ | Split						-- Surjection of the retract
+ | BoxT							-- Generalize to the untyped universe
+ | Unbox						-- Specialize the untype universe to a specific type
+ -- | Gen Type                     -- Generalize
+ -- | Spec Type                    -- Specialize   
  | Fun Type (Bind Vnm Term)     -- \lambda-abstraction
  | App Term Term                -- Function application
  | Pair Term Term               -- Pairs
@@ -32,17 +36,28 @@ data Term =
  | Succ Term                    -- Successor of a natural number
  | Zero                         -- The natural number 0
  deriving Show
+ 
+data TypeGen =
+	Type						-- Terminating types
+ |	Qmark						-- Untyped Universe
+ |	TypeFunc TypeGen TypeGen	-- Function type
+ deriving Show
+
+	
+	
 
 -- Derives all of the meta-machinary like substitution and
 -- \alpha-equality for us.
-$(derive [''Term,''Type])
+$(derive [''Term,''Type,TypeGen])
 instance Alpha Term
 instance Alpha Type
+instance Alpha TypeGen
 
 instance Subst Term Type
 instance Subst Term Term where
   isvar (Var x) = Just (SubstName x)
   isvar _ = Nothing
+instance Subst Term TypeGen
 
 n2s :: Name a -> String
 n2s = name2String
