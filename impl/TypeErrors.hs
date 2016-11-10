3aa05d9ac9290d24e76c13c42043553c0c2fffee
michael_townsend <michael.h.townsend@gmail.com>
2016-11-09 22:09:07 -0500

    added more examples, split them into two files

--- impl/TypeErrors.hs
+++ impl/TypeErrors.hs
@@ -24,9 +24,7 @@ data TypeError = FreeVarsError Vnm
 readTypeError :: TypeError -> String
 readTypeError (FreeVarsError a) =
     "Type error: variable " ++(n2s a) ++ " is free, but I can only typecheck closed terms."
-readTypeError (SuccError a) = do
-                                pt <- prettyTerm a
-                                "Type error (successor): " ++pt
+readTypeError (SuccError a) = "Type error (successor)" 
 readTypeError (FstError a) = "Type error(first projection)"
 readTypeError (SndError a) = "Type error (second projection)"
 readTypeError (FunError a) = "Type error (function)"
@@ -35,7 +33,3 @@ readTypeError (NoTypeError a) = "Type error: No type was found"
 readTypeError (UnMatchedTypes a b) = "Type error: "++ (prettyType a) ++" must have the correct type with "++ (prettyType b)
 readTypeError (BoxError a) = "Type error: You cannot box "++ (prettyType a)++", you can only box terminating types"
 readTypeError (UnboxError a) = "Type error: You cannont unbox "++ (prettyType a)++", you can only unbox terminating types"
-
--- readTypeError (SuccError a) = do
-                        -- pt <- prettyTerm a
-                        -- "Type error (successor): " ++pt 
