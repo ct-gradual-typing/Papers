f4367845c4fe34b51754cb60c4ea6c187238cd29
Harley Eades <heades@gru.edu>
2017-01-10 17:46:05 -0500

    Finally got the Y combinator working.
    Checkout Examples/Naturals.gry and see the definition of natural number
    addition.  Add, drumrole please,
    
    Grady> :l Examples/Naturals.gry
    Grady> add 3 6
    9
    Grady>

--- impl/Eval.hs
+++ impl/Eval.hs
@@ -3,6 +3,7 @@ module Eval where
 import Syntax
 import TypeChecker
 import TypeErrors
+import Pretty
 
 type EM = ExceptT TypeError LFreshM
 
@@ -13,12 +14,12 @@ evalTerm :: Term -> EM Term
 evalTerm t = infer t >> evalTerm' t
 
 evalTerm' :: Term -> EM Term
-evalTerm' (Fun ty b) = lunbind b $ (\(x,t) -> evalTerm' t >>= (\e -> return $ Fun ty $ bind x e))
-evalTerm' (TFun ty b) = lunbind b $ (\(x,t) -> evalTerm' t >>= (\e -> return $ TFun ty $ bind x e))
+
 evalTerm' (Pair t1 t2) = do
   e1 <- evalTerm' t1
   e2 <- evalTerm' t2
   return $ Pair e1 e2
+
 evalTerm' (Fst t) = do
   e <- evalTerm' t
   return $ case e of
@@ -32,24 +33,20 @@ evalTerm' (Snd t) = do
 evalTerm' (Succ t) = evalTerm' t >>= (\e -> return $ Succ e)
 
 evalTerm' (NCase t t1 b) = lunbind b $ (\(x,t2) ->
-   do e <- evalTerm' t
-      e1 <- evalTerm' t1
-      e2 <- evalTerm' t2
+   do e <- evalTerm' t            
       case e of
-        Zero -> return e1
-        Succ e' -> evalTerm' $ subst x e' e2
-        _ -> return $ NCase e e1 (bind x e2))
+        Zero -> evalTerm' t1
+        Succ e' -> (evalTerm' t2) >>= (evalTerm'.(subst x e'))
+        _ -> return $ NCase e t1 (bind x t2))
 
-evalTerm' (LCase t t1 b) =
-    lunbind b $ (\(x,b') ->
-       lunbind b' $ (\(y,t2) ->
+evalTerm' (LCase t t1 b) =    
            do e <- evalTerm' t
-              e1 <- evalTerm' t1
-              e2 <- evalTerm' t2
               case e of
-                Empty -> return e1
-                Cons e' et -> evalTerm' $ subst x e' (subst y et e2)
-                _ -> return $ LCase e e1 (bind x (bind y e2))))
+                Empty -> evalTerm' t1
+                Cons e' et ->
+                    lunbind b  $ (\(x,b') ->
+                    lunbind b' $ (\(y,t2) -> (evalTerm' t2) >>= (evalTerm'.(subst x e').(subst y et))))
+                _ -> return $ LCase e t1 b
 
 evalTerm' (Cons h t) = do
   e <- evalTerm' h
@@ -58,22 +55,31 @@ evalTerm' (Cons h t) = do
 
 evalTerm' (App t1 t2) = do
   e1 <- evalTerm' t1
-  e2 <- evalTerm' t2
   case e1 of
-    Fun ty b -> lunbind b $ (\(x,e) -> evalTerm' $ subst x e2 e)
-    Split ty -> case e2 of
-                  App (Squash ty') e2' -> if ty `aeq` ty'
-                                          then return e2'
-                                          else throwError $ SplitSquashTypeError ty ty'
-                  _ -> return $ App e1 e2
-    Unbox ty -> case e2 of
-                  App (Box ty') e2' -> do
+    Fun ty b -> lunbind b $ (\(x,e) -> evalTerm' $ subst x t2 e)
+    Split ty -> do
+        e2 <- evalTerm' t2
+        case e2 of
+          App (Squash ty') e2' -> if ty `aeq` ty'
+                                  then return e2'
+                                  else throwError $ SplitSquashTypeError ty ty'
+          _ -> return $ App e1 e2
+    Unbox ty -> do
+        e2 <- evalTerm' t2
+        case e2 of
+          App (Box ty') e2' -> do
                           at2 <- infer e2'
                           if (getType at2) `aeq` ty
                           then return e2'
                           else throwError $ UnboxBoxTypeError (getType at2) ty
-                  _ -> return $ App e1 e2
-    _ -> return $ App e1 e2
+          _ -> return $ App e1 e2
+    Box ty -> do
+      e2 <- evalTerm' t2
+      return $ App e1 e2
+    Squash ty -> do
+      e2 <- evalTerm' t2
+      return $ App e1 e2
+    _ -> return $ App e1 t2
 evalTerm' (TApp ty t) = do
   e <- evalTerm' t
   case e of
